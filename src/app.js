96be2c54b4d3f2f13cdb47c7c5b754e7d24d4e0f
varshitha11104 <bhumireddyvarshitha944@gmail.com>
2025-07-03 11:38:23 -1200

    writing routes for post,get..

--- src/app.js
+++ src/app.js
@@ -2,9 +2,20 @@ const express=require('express');
 
 const app=express();
 
-app.use((req,res)=>{
-    res.send("Hello from the server");
+//responds to only post
+app.post("/user",(req,res)=>{
+    res.send("user in post");
 })
+
+//responds to only get
+app.get("/user",(req,res)=>{
+    res.send("user in get");
+})
+
+app.use("/user",(req,res)=>{  //responds to both get and post
+    res.send("inside helo");
+})
+
 app.listen(3000,()=>{
     console.log('server is listening at 3000...');
 });
