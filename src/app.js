1528dfb64eda1b7790f27c644b6192deaceaed58
varshitha11104 <bhumireddyvarshitha944@gmail.com>
2025-07-03 11:44:02 -1200

    multiple request handlers

--- src/app.js
+++ src/app.js
@@ -2,19 +2,18 @@ const express=require('express');
 
 const app=express();
 
-//responds to only post
-app.post("/user",(req,res)=>{
-    res.send("user in post");
-})
-
-//responds to only get
-app.get("/user",(req,res)=>{
-    res.send("user in get");
-})
-
-app.use("/user",(req,res)=>{  //responds to both get and post
-    res.send("inside helo");
-})
+//writing multiple request handlers
+//app.use("/route",rh1,rh2,rh3)
+app.use("/user",(req,res,next)=>{  //responds to both get and post
+    console.log("request handler1");
+    //res.send("Response 1");
+    next();
+},
+(req,res)=>{
+    console.log("request handler 2");  //this will also execute while next() called
+    res.send("response 2"); //shows error because tcp connection got closed after sending first reponse
+}
+);
 
 app.listen(3000,()=>{
     console.log('server is listening at 3000...');
